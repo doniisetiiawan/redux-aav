a1b55c137b2fd16d51dfd1da752545657149ca52
Doni <donisetiawan.050@gmail.com>
2019-07-12 17:26:50 +0700

    Understanding redux method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import { createStore } from 'redux';
+
 import logo from './logo.svg';
 import './App.css';
+
+const tubeLight = (state = 'OFF', action) => {
+  switch (action.type) {
+    case 'TURN_ON':
+      return 'ON';
+    case 'TURN_OFF':
+      return 'OFF';
+    default:
+      return state;
+  }
+};
+
+const store = createStore(tubeLight);
+console.log('Initially tubelight is: ', store.getState());
+
+store.dispatch({ type: 'TURN_ON' });
+console.log('Now tubelight is: ', store.getState());
 
 function App() {
   return (
