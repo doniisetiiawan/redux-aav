b7376ee0a1f12a457a45341fb9cfde5e76e25431
Doni <donisetiawan.050@gmail.com>
2019-07-13 14:33:15 +0700

    Testing react-router-dom

--- src/containers/HomePage/index.jsx
+++ src/containers/HomePage/index.jsx
@@ -1,7 +1,31 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 
-const HomePage = () => (
-  <h1>This is the HomePage Redux-book container!</h1>
+const MenuLink = ({ to, ...rest }) => (
+  <Route path={to}>
+    {({ match }) => (
+      <li className={match ? 'active' : ''}>
+        <Link to={to} {...rest} />
+      </li>
+    )}
+  </Route>
 );
 
-export default HomePage;
+const Homepage = () => (
+  <div>
+    <h1>This is home page our our application.</h1>
+    <ul>
+      <MenuLink to="/about">About Us</MenuLink>
+      <MenuLink to="/login">Login</MenuLink>
+      <MenuLink to="/register">Register</MenuLink>
+      <MenuLink to="/contact">Contact</MenuLink>
+    </ul>
+  </div>
+);
+
+export default Homepage;
+
+MenuLink.propTypes = {
+  to: PropTypes.string,
+};
