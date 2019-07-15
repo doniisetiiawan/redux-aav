d3a6f12e8176c4cc38df2597dbc7a10b1fb699ff
Doni <donisetiawan.050@gmail.com>
2019-07-15 12:48:23 +0700

    HPF

---
+++ src/utils/injectReducer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import hoistNonReactStatics from 'hoist-non-react-statics';
+
+import getInjectors from './reducerInjectors';
+
+export default ({ key, reducer }) => (WrappedComponent) => {
+  class ReducerInjector extends React.Component {
+    static WrappedComponent = WrappedComponent;
+
+    static contextTypes = {
+      store: PropTypes.object.isRequired,
+    };
+
+    static displayName = `withReducer(${WrappedComponent.displayName
+    || WrappedComponent.name
+    || 'Component'})`;
+
+    // eslint-disable-next-line react/destructuring-assignment
+    injectors = getInjectors(this.context.store);
+
+    componentWillMount() {
+      const { injectReducer } = this.injectors;
+
+      injectReducer(key, reducer);
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  }
+
+  return hoistNonReactStatics(ReducerInjector, WrappedComponent);
+};
